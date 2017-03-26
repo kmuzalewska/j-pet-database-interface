e48c83abf43e00773ac68fad4ffee07ec8ffc045
kmuzalewska <klara.muzalewska@gmail.com>
2017-03-27 00:47:18 +0200

    Universalization
    Trying to create form and function to create foems without making new form; still falling

--- JPET/views.py
+++ JPET/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import redirect
-from .forms import *
+from django.forms import modelform_factory
 from .models import *
-
+from .forms import UniversalForm
 
 # Create your views here.
 def index(request):
@@ -27,41 +27,22 @@ def faq(request):
     return HttpResponse(template.render(request))
 
 
-def scin(request):
-    if request.method == "POST":
-        form = ScinForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            return redirect('index')
-    else:
-        form = ScinForm()
-    return render(request, 'universal.html', {'form': form})
-
-
-def scinType(request):
-    if request.method == "POST":
-        form = ScinTypeForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            return redirect('index')
-    else:
-        form = ScinTypeForm()
-    return render(request, 'universal.html', {'form': form})
-
-
 D = {'scin/': Scin, 'scintype/': ScinType}
-
+fieldsList = ['name','description', 'type', 'length', 'width', 'height ']
 
 def universal(request, nazwa_modelu):
     try:
-        if request.method == "POST":
-            form = UniversalForm(request.POST, model=D[nazwa_modelu])
-            if form.is_valid():
-                post = form.save(commit=False)
-                return redirect('index')
-        else:
-            form = UniversalForm(request, model=D[nazwa_modelu])
-        return render(request.GET, 'universal.html', {'form': form})
+        #if request.method == "POST":
+        form = UniversalForm(request.POST, nazwa_modelu)
+        print nazwa_modelu
+        if form.is_valid():
+            post = form.save(commit=False)
+            return redirect('index')
+        #else:
+            #form = UniversalForm(nazwa_modelu)
+        return render(request, 'universal.html', {'form': form})
     except KeyError:
         print nazwa_modelu
+        print 'hello'
     # jak dac 404
+

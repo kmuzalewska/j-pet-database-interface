ef50ee56f7db87f5ac718719a3f21312d7901fef
kmuzalewska <klara.muzalewska@gmail.com>
2017-05-16 11:49:08 +0200

    added forms to all models and universal view to read all forms

--- JPET/forms.py
+++ JPET/forms.py
@@ -11,3 +11,176 @@ class LoginForm(AuthenticationForm):
     password = forms.CharField(widget=PasswordInput(attrs={'class': 'mdl-textfield__input', 'placeholder': 'Password'}))
 
 
+# dict_of_all_classes = {'scintype':ScinType, 
+#                         'scin':Scin}
+
+
+# class UniversalForm(forms.Form):
+#     def __init__(self, model_name):
+#         super(UniversalForm, self).__init__(*args, **kwargs)
+#         self.model_name = model_name
+#         self.f = dict_of_all_classes[self.model_name]
+#         name = self.f.__name__.lower()
+#         setattr(self, name, self.f(*args, **kwargs))
+#         form = getattr(self, name)
+#         self.fields.update(form.fields)
+#         self.initial.update(form.initial)
+
+#     def fieldsForModel(self, model_name):
+#         self.modelFields = dict_of_all_classes[model_name]._meta.fields
+#         newmodel = []
+#         for i in modelFields:
+#             newmodel.append(i.name)
+#         return newmodel
+
+class ScinTypeForm(forms.ModelForm):
+
+    class Meta:
+        model = ScinType
+        fields = '__all__'
+
+
+class ScinForm(forms.ModelForm):
+
+    class Meta:
+        model = Scin
+        fields = '__all__'
+
+
+class SetupForm(forms.ModelForm):
+
+    class Meta:
+        model = Setup
+        fields = '__all__'
+
+
+class StatusTypeForm(forms.ModelForm):
+
+    class Meta:
+        model = StatusType
+        fields = '__all__'
+
+
+class StatusForm(forms.ModelForm):
+
+    class Meta:
+        model = Status
+        fields = '__all__'
+
+
+class FrameForm(forms.ModelForm):
+
+    class Meta:
+        model = Frame
+        fields = '__all__'
+
+
+class LayerForm(forms.ModelForm):
+
+    class Meta:
+        model = Layer
+        fields = '__all__'
+
+
+class SlotForm(forms.ModelForm):
+
+    class Meta:
+        model = Slot
+        fields = '__all__'
+
+
+class ScinInsertetForm(forms.ModelForm):
+
+    class Meta:
+        model = ScinInsertet
+        fields = '__all__'
+
+
+class PMModelForm(forms.ModelForm):
+
+    class Meta:
+        model = PMModel
+        fields = '__all__'
+
+
+class PMForm(forms.ModelForm):
+
+    class Meta:
+        model = PM
+        fields = '__all__'
+
+
+class HVForm(forms.ModelForm):
+
+    class Meta:
+        model = HV
+        fields = '__all__'
+
+class HVChannelForm(forms.ModelForm):
+
+    class Meta:
+        model = HVChannel
+        fields = '__all__'
+
+
+class SideForm(forms.ModelForm):
+
+    class Meta:
+        model = Side
+        fields = '__all__'
+
+
+class PMInsertedForm(forms.ModelForm):
+
+    class Meta:
+        model = PMInserted
+        fields = '__all__'
+
+
+class RunForm(forms.ModelForm):
+
+    class Meta:
+        model = Run
+        fields = '__all__'
+
+
+class MeasurementTypeForm(forms.ModelForm):
+
+    class Meta:
+        model = MeasurementType
+        fields = '__all__'
+
+
+class MeasurementForm(forms.ModelForm):
+
+    class Meta:
+        model = Measurement
+        fields = '__all__'
+
+
+class MeasurementForm(forms.ModelForm):
+
+    class Meta:
+        model = Measurement
+        fields = '__all__'
+
+
+class RadiationSourceTypeForm(forms.ModelForm):
+
+    class Meta:
+        model = RadiationSourceType
+        fields = '__all__'
+
+
+class RadiationSourceForm(forms.ModelForm):
+
+    class Meta:
+        model = RadiationSource
+        fields = '__all__'
+
+
+class RadiationSourceInsertedForm(forms.ModelForm):
+
+    class Meta:
+        model = RadiationSourceInserted
+        fields = '__all__'
--- JPET/views.py
+++ JPET/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import redirect
-from django.forms import modelform_factory
+#from django.forms import modelform_factory
 from .models import *
+from .forms import *
 from django.http import HttpResponse, HttpResponseNotFound
 from django.forms import Textarea 
 # Create your views here.
@@ -26,25 +27,48 @@ def faq(request):
     template = loader.get_template('faq.html')
     return HttpResponse(template.render(request))
 
-dict_of_all_classes = {'scintype':ScinType, 
-                        'scin':Scin}
+# dict_of_all_classes = {'scintype':ScinType, 
+#                         'scin':Scin}
 
-def fieldsForModel(model_name):
-    modelFields = dict_of_all_classes[model_name]._meta.fields
-    newmodel = []
-    for i in modelFields:
-        newmodel.append(i.name)
-    return newmodel
+dict_of_all_form = {'scintype':ScinTypeForm,
+                    'scin':ScinForm,
+                    'setup':SetupForm,
+                    'statustype':StatusTypeForm,
+                    'status':StatusForm,
+                    'frame':FrameForm,
+                    'layer':LayerForm,
+                    'slot':SlotForm,
+                    'scininserted':ScinInsertetForm,
+                    'pmmodel':PMModelForm,
+                    'pm':PMForm,
+                    'hv':HVForm,
+                    'hvchannel':HVChannelForm,
+                    'side':SideForm,
+                    'pminserted':PMInsertedForm,
+                    'run':RunForm,
+                    'measurementtype':MeasurementTypeForm,
+                    'measurement':MeasurementForm,
+                    'radiationsourcetype':RadiationSourceTypeForm,
+                    'radiationsource':RadiationSourceForm,
+                    'radiationsourceinserted':RadiationSourceInsertedForm
+                    }
+# def fieldsForModel(model_name):
+#     modelFields = dict_of_all_classes[model_name]._meta.fields
+#     newmodel = []
+#     for i in modelFields:
+#         newmodel.append(i.name)
+#     return newmodel
 
 
 def universal(request, model_name):
     try:
-        f =modelform_factory(dict_of_all_classes[model_name], fields=fieldsForModel(model_name))
+        #f =modelform_factory(dict_of_all_classes[model_name], fields=fieldsForModel(model_name=model_name))
         if request.method == "POST":
             print 'dffhghgfs'
-            form = f()
+            form = dict_of_all_form[model_name](request.POST)
+            post = form.save()
             if form.is_valid():
-                post = form.save()
+                # post = form.save()
                 print 'dffhs'
                 post.save()
                 return render(request, 'universal.html', {'form': form})
@@ -52,7 +76,7 @@ def universal(request, model_name):
                 print 'dffasshs'
                 return render(request, 'universal.html', {'form': form})
         else:
-            form = f()
+            form = dict_of_all_form[model_name]()
             print 'dfs'
             return render(request, 'universal.html', {'form': form})
     except KeyError:
